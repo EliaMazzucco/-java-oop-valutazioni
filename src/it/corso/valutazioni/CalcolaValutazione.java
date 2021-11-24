e163f859197077ac905fa88dd81ee697a92a855e
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-24 15:49:00 +0100

    secondo commit

--- src/it/corso/valutazioni/CalcolaValutazione.java
+++ src/it/corso/valutazioni/CalcolaValutazione.java
@@ -1,5 +1,44 @@
 package it.corso.valutazioni;
 
 public class CalcolaValutazione {
+	
+	
+	/*/Classe CalcolaValutazione con tre attributi: id dello studente (intero), percentuale assenze (intero), media voti (numero decimale). 
+	 * La classe deve avere anche un metodo che calcola, in base a percentuale assenze e media voti, se lo studente è promosso o bocciato
+	 * 
+	 * 
+	 */
 
+	//proprietà
+	int idStudente;
+	int percAssenze;
+	double mediaVoti;
+	
+	//Costruttore
+	
+	CalcolaValutazione (int idStudente, int percAssenze, double mediaVoti) {
+		this.idStudente = idStudente;
+		this.percAssenze =percAssenze;
+		this.mediaVoti = mediaVoti;
+		
+	}
+	
+	//Metodi
+	
+	
+	//Calcolo promosos o bocciato
+	
+	/*/
+	 * se lo studente ha fatto più del 50% di assenze è bocciato
+	   se ha fatto tra il 25% e il 50% di assenze è promosso solo se la media dei voti è superiore a 2
+	   se ha fatto meno del 25% di assenze è promosso se la media dei voti è superiore o uguale a 2
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	
 }
--- src/it/corso/valutazioni/Main.java
+++ src/it/corso/valutazioni/Main.java
@@ -1,10 +1,34 @@
 package it.corso.valutazioni;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		/*/
+		 * Classe Main con metodo main in cui create un array di 20 CalcolaValutazione, lo popolate con id studente progressivo (da 1 a 20), percentuale assenze e media voti generati random. 
+		 * Per ogni elemento dell’array stampate a video l’id studente e se è promosso o bocciato
+		 * 
+		 */
+		
+		CalcolaValutazione [] calcolaValutazione = new CalcolaValutazione[20];
+		Random randomGenerator = new Random();
+		
+		int idStudente, percAssenze;
+		double mediaVoti;
+		
+		for (int i = 0; i < calcolaValutazione.length; i++) {
+			idStudente = randomGenerator.nextInt(20) + 1;
+			percAssenze  = randomGenerator.nextInt(100);
+			mediaVoti = randomGenerator.nextInt(5);
+			
+			calcolaValutazione[i] = new CalcolaValutazione(idStudente, percAssenze, mediaVoti);
+			
+			System.out.println(calcolaValutazione[i]);
+		}
+		
+		
 	}
 
 }
